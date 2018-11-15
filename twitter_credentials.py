5050915d4474ee0bb214b7168f3bc23afb7030a5
NicolasGDM <34142471+NicolasGDM@users.noreply.github.com>
2018-11-15 00:54:04 -0500

    Update twitter_credentials.py

--- twitter_credentials.py
+++ twitter_credentials.py
@@ -1,16 +1,9 @@
 ################################################################################
 ##################### Fill up you Twitter Credentials ##########################
-# twitter_auth = {
-#     'consumer_key': 'REPLACE_WITH_YOUR_CONSUMER_KEY',
-#     'consumer_secret': 'REPLACE_WITH_YOUR_CONSUMER_SECRET_KEY',
-#     'access_token': 'REPLACE_WITH_YOUR_ACCESS_TOKEN',
-#     'access_token_secret': 'REPLACE_WITH_YOUR_ACCESS_SECRET_TOKEN',
-#   }
+ twitter_auth = {
+     'consumer_key': 'REPLACE_WITH_YOUR_CONSUMER_KEY',
+     'consumer_secret': 'REPLACE_WITH_YOUR_CONSUMER_SECRET_KEY',
+     'access_token': 'REPLACE_WITH_YOUR_ACCESS_TOKEN',
+     'access_token_secret': 'REPLACE_WITH_YOUR_ACCESS_SECRET_TOKEN',
+   }
 
-# # 
-twitter_auth = {
-    'consumer_key': '8vCs3h48gj9nZ5WHBrBIzmwmk',
-    'consumer_secret': 'xLhvzWIfv5cfiDwpFvrpsMCxi5cVoo6ii58HOvuKHL86PytIG1',
-    'access_token': '848171362019672065-gw7B7I3nNJx0JgLc9JzFit2CblFgPob',
-    'access_token_secret': 'sRZevSaxw5A6LkiY9IJvMT8kY63VpHXBisiAiWhULxd9x',
-  }
